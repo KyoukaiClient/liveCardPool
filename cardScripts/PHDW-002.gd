890c2c2e53275a131a38701b5d49d3cd5f88cb96
hardtospellwrong <43374347+hardtospellwrong@users.noreply.github.com>
2024-11-02 01:00:33 -0400

    Switch to Zips

--- cardScripts/PHDW-002.gd
+++
@@ -1,2 +0,0 @@
-extends Node2D
-#Sui, Spellshard Researcher
--- cardScripts/PHDW-007.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-#Hunting Eagle
-
-func resoForOpenMe():
-	get_parent().get_parent().readyMe()
-	var snapZoneNode
-	snapZoneNode = get_node(Global.pathStrings["cursorMouseCatch"]+"/"+get_parent().get_parent().inZoneID)
